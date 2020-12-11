240d3299f54a085eaf6232860ae6686208b0fd49
Tiago Boeing <contato@tiagoboeing.com>
2020-12-11 18:26:26 -0300

    Create Stencil project

---
+++ stencil.config.ts
@@ -0,0 +1,41 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'tecnologia-webcomponents',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+      copy: [
+        {
+          src: 'styles.css',
+        },
+      ],
+    },
+  ],
+  globalStyle: 'src/scss/global.scss',
+  plugins: [
+    sass({
+      injectGlobalPaths: ['src/scss/injected.scss'],
+    }),
+  ],
+  testing: {
+    collectCoverage: true,
+    /**
+     * Gitlab CI doesn't allow sandbox, therefor this parameters must be passed to your Headless Chrome
+     * before it can run your tests
+     */
+    browserArgs: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions'],
+  },
+};
