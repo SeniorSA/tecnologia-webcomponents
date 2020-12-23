627eabcdca858c39a73851a71b01ce0e6bf1dd1c
Lucas Reichert <lucasreichert3@gmail.com>
2020-12-23 13:00:54 -0300

    feature: #4 rating satisfaction component

--- src/components/rating-satisfaction/rating-satisfaction.tsx
+++ src/components/rating-satisfaction/rating-satisfaction.tsx
@@ -24,6 +24,7 @@ export class RatingSatisfaction {
 
   getClassByGrade(grade: number) {
     switch (grade) {
+      case 0:
       case 1:
         return 'one'
       case 2:
@@ -37,13 +38,31 @@ export class RatingSatisfaction {
     }
   }
 
+  getRatingIcon(): string {
+    const rate = Math.round(this.rate)
+    switch (rate) {
+      case 0:
+      case 1:
+        return 'fa-frown-open';
+      case 2:
+        return 'fa-frown';
+      case 3:
+        return 'fa-meh';
+      case 4:
+        return 'fa-smile';
+      case 5:
+        return 'fa-laugh-beam';
+    }
+  }
+
   render() {
     return (
       <Host>
         <div class="container">
           {this.allGrades.map(grade => (<div class={`grade ${this.getContainerClass(grade)}`}></div>))}
           <div class="icon">
-            <i class={`${this.iconPrefix} fa-smile`}></i>
+            <i class={`${this.iconPrefix} ${this.getRatingIcon()}`}></i>
+            <div class="icon-background"></div>
           </div>
         </div>
       </Host>
