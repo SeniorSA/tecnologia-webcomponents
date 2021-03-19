db46dde7254b47dbbd270f6760cbff2d42fca972
Tiago Boeing <contato@tiagoboeing.com>
2021-03-18 23:59:14 -0300

    Fix CF path

--- scripts/storybook-deploy.sh
+++ scripts/storybook-deploy.sh
@@ -9,10 +9,10 @@ aws s3 sync storybook-static ${BUCKET}/master --delete
 echo "Copy to S3 done!"
 echo "Preparing to invalidade cache on AWS CloudFront"
 
-aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_LANDINGPAGE_DIST_ID} --paths "master/*"
+aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_LANDINGPAGE_DIST_ID} --paths "/master/*"
 
 # Create a isolated folder to version
 if [ ! -f $VERSION ]; then
   aws s3 sync storybook-static ${BUCKET}/${VERSION} --delete
-  aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_LANDINGPAGE_DIST_ID} --paths "#{$VERSION}/*"
+  aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_LANDINGPAGE_DIST_ID} --paths "/${VERSION}/*"
 fi
