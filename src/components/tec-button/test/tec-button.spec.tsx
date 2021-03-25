0738bdf19db075cb38068202c0aa84b1cdc50731
Lucas Reichert <lucasreichert3@gmail.com>
2021-03-25 08:49:18 -0300

    add test

--- src/components/tec-button/test/tec-button.spec.tsx
+++ src/components/tec-button/test/tec-button.spec.tsx
@@ -2,13 +2,27 @@ import { newSpecPage } from '@stencil/core/testing';
 import { TecButton } from '../tec-button';
 
 describe('tec-button', () => {
-  it('renders', async () => {
+  it('renders theme light', async () => {
     const page = await newSpecPage({
       components: [TecButton],
       html: `<tec-button></tec-button>`,
     });
     expect(page.root).toEqualHtml(`
-      <tec-button>
+      <tec-button theme="light">
+        <mock:shadow-root>
+          <button type="button" class="solid rounded primary text-sans text-base"><span class=""></span></button>
+        </mock:shadow-root>
+      </tec-button>
+    `);
+  });
+
+  it('renders theme dark', async () => {
+    const page = await newSpecPage({
+      components: [TecButton],
+      html: `<tec-button theme="dark"></tec-button>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <tec-button theme="dark">
         <mock:shadow-root>
           <button type="button" class="solid rounded primary text-sans text-base"><span class=""></span></button>
         </mock:shadow-root>
@@ -22,7 +36,7 @@ describe('tec-button', () => {
       html: `<tec-button buttonId="1" label="Confirmar" size="small" mode="rounded"></tec-button>`,
     });
     expect(page.root).toEqualHtml(`
-      <tec-button>
+      <tec-button buttonid="1" label="Confirmar" mode="rounded" size="small" theme="light">
         <mock:shadow-root>
           <button type="button" id="1" class="solid rounded primary text-sans text-base"><span class="">Confirmar</span></button>
         </mock:shadow-root>
