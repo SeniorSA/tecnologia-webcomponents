c6d756c3edcff18048f767ba527b6341b1884a0c
Tiago Boeing <contato@tiagoboeing.com>
2021-04-02 00:05:10 -0300

    [skip ci] Partial unit test

--- src/components/inputs/code-input/test/code-input.e2e.ts
+++ src/components/inputs/code-input/test/code-input.e2e.ts
@@ -1,11 +1,27 @@
 import { newE2EPage } from '@stencil/core/testing'
 
-describe('code-input', () => {
+describe('tec-code-input', () => {
   it('renders', async () => {
     const page = await newE2EPage()
-    await page.setContent('<code-input></code-input>')
+    await page.setContent('<tec-code-input></tec-code-input>')
 
-    const element = await page.find('code-input')
+    const element = await page.find('tec-code-input')
     expect(element).toHaveClass('hydrated')
+  })
+
+  it('should show placeholder', async () => {
+    const page = await newE2EPage()
+    await page.setContent('<tec-code-input placeholder="01234"></tec-code-input>')
+
+    const element = await page.find('tec-code-input')
+    expect(element).toHaveAttribute('placeholder')
+    expect(element.getAttribute('placeholder')).toEqual('01234')
+
+    for (let index = 0; index < 5; index++) {
+      expect(
+        (element.shadowRoot.querySelector(`input#field-${index}`) as HTMLInputElement)
+          .placeholder
+      ).toEqual(`${index}`)
+    }
   })
 })
